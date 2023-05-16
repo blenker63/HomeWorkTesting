d2d2f5ac49479689df4a78b78fa0d5377042b50f
vaceslavkameronov <slka@mail.ru>
2023-05-16 12:19:17 +0400

    Cоздание проекта ДЗ по теме "Тестирование".

---
+++ src/main/java/com/prosky/homeworktesting/HomeWorkTestingApplication.java
@@ -0,0 +1,13 @@
+package com.prosky.homeworktesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeWorkTestingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeWorkTestingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/prosky/homeworktesting/HomeWorkTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prosky.homeworktesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeWorkTestingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
