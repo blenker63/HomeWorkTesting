fd5e8a3579aab43703f431d31dd52ecfe41eb387
vaceslavkameronov <slka@mail.ru>
2023-05-19 20:53:49 +0400

    выполнение ДЗ

--- src/test/java/com/prosky/homeworktesting2/service/CalculatorTest.java
+++ src/test/java/com/prosky/homeworktesting2/service/CalculatorTest.java
@@ -4,43 +4,51 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class CalculatorTest {
-    double num1 = 5;
-    double num2 = 5;
     private final CalculatorService calculatorService = new CalculatorService();
 
     @Test
     public void calculatorPlusTest() {
-        double actual = calculatorService.plusCalculator(num1, num2);
-        double expected = num1 + num2;
-        Assertions.assertEquals(actual, expected);
+        double actual1 = calculatorService.plusCalculator(5 , 5);
+        Assertions.assertEquals(10 , actual1);
+        double actual2 = calculatorService.plusCalculator(-2 , 5);
+        Assertions.assertEquals(3 , actual2);
+        double actual3 = calculatorService.plusCalculator(2 , -5);
+        Assertions.assertEquals(-3 , actual3);
     }
 
     @Test
     public void calculatorMinusTest() {
-        double actual = calculatorService.minusCalculator(num1, num2);
-        double expected = num1 - num2;
-        Assertions.assertEquals(actual, expected);
+        double actual1 = calculatorService.minusCalculator(5 , 5);
+        Assertions.assertEquals(actual1, 0);
+        double actual2 = calculatorService.minusCalculator(-2 , 5);
+        Assertions.assertEquals(actual2, -7);
+        double actual3 = calculatorService.minusCalculator(2 , -5);
+        Assertions.assertEquals(actual3, 7);
     }
 
     @Test
     public void calculatorMultiplyTest() {
-        double actual = calculatorService.multiplyCalculator(num1, num2);
-        double expected = num1 * num2;
-        Assertions.assertEquals(actual, expected);
+        double actual1 = calculatorService.multiplyCalculator(5 , 5);
+        Assertions.assertEquals(25 , actual1);
+        double actual2 = calculatorService.multiplyCalculator(-2 , 5);
+        Assertions.assertEquals(-10 , actual2);
+        double actual3 = calculatorService.multiplyCalculator(-2 , -5);
+        Assertions.assertEquals(10 , actual3);
     }
 
     @Test
     public void calculatorDivideTest() {
-        double actual = calculatorService.divideCalculator(num1, num2);
-        double expected = num1 / num2;
-        Assertions.assertEquals(actual, expected);
+        double actual1 = calculatorService.divideCalculator(5 , 5);
+        Assertions.assertEquals(1 , actual1);
+        double actual2 = calculatorService.divideCalculator(-2 , 5);
+        Assertions.assertEquals(-0.4 , actual2);
+        double actual3 = calculatorService.divideCalculator(-2 , -5);
+        Assertions.assertEquals(0.4 , actual3);
     }
 
     @Test
     public void calculatorDivideExceptionTest() {
-        num2 = 0;
         Assertions.assertThrows(IllegalArgumentException.class,
-        ( ) -> calculatorService.divideCalculator(num1, num2));
+        ( ) -> calculatorService.divideCalculator(5 , 0));
     }
-
 }
